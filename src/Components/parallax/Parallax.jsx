ab9904a8de326781b361747d378f21f23acdd24a
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-22 23:41:07 +0530

    parallex changes

--- src/Components/parallax/Parallax.jsx
+++ src/Components/parallax/Parallax.jsx
@@ -19,7 +19,7 @@ const Parallax = ({type}) => {
     style={{background: type==="services"
       ? "linear-gradient(180deg, #111132, #0c0c1d"
       : "linear-gradient(180deg, #111132, #505064"}}>
-      <motion.h1 style={{y: yText}}>{type === "services" ? "Who I Am?" : "Who I Did?"}</motion.h1>
+      <motion.h1 style={{y: yText}}>{type === "services" ? "Who I Am?" : "What I Did?"}</motion.h1>
       <motion.div className="mountains" style={{backgroundImage: "url(mountains.png)"}}></motion.div>
       <motion.div className="planets" style={{y: yBg, backgroundImage: `url(${type === "services" ? "planets.png" : "sun.png"})`}}></motion.div>
       <motion.div style={{x: yBg, backgroundImage: "url(stars.png)"}} className="stars"></motion.div>
