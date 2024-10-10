9b0d828069422263b120fa00162a5523977046bc
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-10 23:21:05 +0530

    portfolio page updated

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -10,13 +10,17 @@ const items = [
     img: "./ChatterBox.png",
     desc: "A social media platform developed using the MERN stack and Redux, designed for seamless chatting and user interaction. Key features include the ability to follow/unfollow users, upload and interact with posts through likes and comments, edit profiles, and receive real-time notifications. Users can also bookmark posts for later viewing. ChatterBox offers a smooth, dynamic experience for social engagement.",
     link: "https://chat-tap.onrender.com/",
+    code: "https://github.com/Vermasaiyam/ChatterBox",
+    technologies: ["TailwindCSS", "React", "Node.js", "Express.js", "MongoDB"],
   },
   {
     id: 2,
     title: "Chat-Tap",
     img: "./ChatTap.png",
-    desc: "Developed a full-featured real-time chat application using the MERN stack (MongoDB, Express, React, Node.js). The app includes user authentication (with JWT), real-time one-on-one and group chats using Socket.io, and additional features like dark mode for an enhanced user experience. Designed with a responsive UI, the app offers seamless navigation, message notifications, and chat history. Built with a focus on performance and scalability, it's optimized for both desktop and mobile devices.",
+    desc: "Developed a full-featured real-time chat application using the MERN stack. The app includes user authentication (with JWT), real-time one-on-one and group chats using Socket.io, and additional features like dark mode for an enhanced user experience. Designed with a responsive UI, the app offers seamless navigation, message notifications, and chat history. Built with a focus on performance and scalability, it's optimized for both desktop and mobile devices.",
     link: "https://chat-tap.onrender.com/",
+    code: "https://github.com/Vermasaiyam/Cartify",
+    technologies: ["Chakra Ui", "React", "Node.js", "Express.js", "MongoDB"],
   },
   {
     id: 3,
@@ -24,6 +28,8 @@ const items = [
     img: "./Ecommerce.png",
     desc: "Developed a E-Commerce web application using only frontend technologies like HTML, CSS, and JavaScript. The project showcases a modern, intuitive UI with a product listing page, product details, cart functionality, and a dynamic checkout experience. Implemented state management for the cart using local storage and ensured smooth navigation using JavaScript-based routing.",
     link: "https://vermasaiyam.github.io/e-commerce/",
+    code: "https://github.com/Vermasaiyam/e-commerce",
+    technologies: ["Bootstrap", "React"],
   },
   {
     id: 4,
@@ -31,6 +37,8 @@ const items = [
     img: "./youtubeApp.png",
     desc: "Built a YouTube-like web application using React and Redux for state management, with API integration to fetch and display video content. The app allows users to search for videos, watch them, view related videos, and manage a watch history. Implemented efficient global state handling with Redux to manage video lists, user actions, and loading states, ensuring a smooth and responsive user experience. The app is fully responsive and optimized for both desktop and mobile devices.",
     link: "https://youtube-black-zeta.vercel.app/",
+    code: "https://github.com/Vermasaiyam/youtube",
+    technologies: ["TailwindCSS", "React"],
   },
   {
     id: 5,
@@ -38,6 +46,8 @@ const items = [
     img: "./spotify.png",
     desc: "Developed a Spotify-inspired music streaming application using only HTML, CSS, and JavaScript. The app allows users to browse, play, and control music through a sleek, modern interface. Implemented audio controls, playlist management, and dynamic updates for playing tracks. With a focus on clean design and intuitive UX, the project replicates key features of a music streaming platform while being fully responsive across devices.",
     link: "https://vermasaiyam.github.io/Spotify-Clone/",
+    code: "https://github.com/Vermasaiyam/Spotify-Clone",
+    technologies: ["HTML", "CSS", "Javascript"],
   },
   {
     id: 6,
@@ -45,12 +55,14 @@ const items = [
     img: "./ticTacToe.png",
     desc: "Created a classic Tic-Tac-Toe game using HTML, CSS, and JavaScript. The game features a simple, clean design with smooth gameplay and logic to track turns, check for winners, and reset the board. The project includes a two-player mode, responsive design for various screen sizes, and an intuitive UI. It demonstrates interactive game mechanics and DOM manipulation with vanilla JavaScript.",
     link: "https://vermasaiyam.github.io/Tic-Tac-Toe/",
+    code: "https://github.com/Vermasaiyam/Tic-Tac-Toe",
+    technologies: ["HTML", "CSS", "Javascript"],
   },
 ]
 
 const Single = ({ item }) => {
   const ref = useRef();
-  const { scrollYProgress } = useScroll({ 
+  const { scrollYProgress } = useScroll({
     target: ref,
     // offset: ["start start", "end start"]
   });
@@ -64,10 +76,20 @@ const Single = ({ item }) => {
           <div className="imageContainer" ref={ref}>
             <img src={item.img} alt={item.title} />
           </div>
-          <motion.div className="textContainer" style={{y}}>
+          <motion.div className="textContainer" style={{ y }}>
             <h2 >{item.title}</h2>
             <p>{item.desc}</p>
-            <a href={item.link} target='blank'><button>See Demo <MdLaunch /></button></a>
+            <div className="tech">
+              {
+                (item.technologies).map(tech => (
+                  <div className="techItem" key={tech}>{tech}</div>
+                ))
+              }
+            </div>
+            <div className="buttons">
+              <a href={item.link} target='blank'><button>See Code<b>&lt; &gt;</b></button></a>
+              <a href={item.link} target='blank'><button>Watch Live <MdLaunch /></button></a>
+            </div>
           </motion.div>
         </div>
       </div>
