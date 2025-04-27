82e7a7342a3097d67b078499a2fdc18cd2c9dd8a
Vermasaiyam <vermasaiyam9@gmail.com>
2025-04-27 14:12:14 +0530

    parallelax done

--- src/Components/parallax/Parallax.jsx
+++ src/Components/parallax/Parallax.jsx
@@ -19,8 +19,8 @@ const Parallax = ({type}) => {
     style={{background: type==="services"
       ? "linear-gradient(180deg, #111132, #0c0c1d"
       : "linear-gradient(180deg, #111132, #505064"}}>
-      <motion.h1 style={{y: yText}}>{type === "services" ? "Who I Am?" : "What I Did?"}</motion.h1>
-      <motion.div className="mountains" style={{backgroundImage: "url(mountains.png)"}}></motion.div>
+      <motion.h1 style={{y: yText, zIndex: 10}}>{type === "services" ? "Who I Am?" : "What I Did?"}</motion.h1>
+      <motion.div className="earth" style={{backgroundImage: "url(earth.png)"}}></motion.div>
       <motion.div className="planets" style={{y: yBg, backgroundImage: `url(${type === "services" ? "planets.png" : "sun.png"})`}}></motion.div>
       <motion.div style={{x: yBg, backgroundImage: "url(stars.png)"}} className="stars"></motion.div>
 
